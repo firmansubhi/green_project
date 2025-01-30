9c6e3bd0b9273d1298459b668633cd4f39775659
firmansubhi <firmansubhi@gmail.com>
2025-01-30 11:40:23 +0700

    db_name

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use("/transactions", transactionRoutes);
 app.use("/payments", paymentRoutes);
 
 // Database connection
-mongoose.connect("mongodb://localhost:27017/recycling", {});
+mongoose.connect("mongodb://localhost:27017/" + process.env.DB_NAME, {});
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "Connection error:"));
